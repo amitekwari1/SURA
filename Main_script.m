de74d258c37907da1b877eb2ffb928102c445f9b
amitekwari1 <amitekwari1@gmail.com>
2021-06-05 19:11:14 +0530

    Add files via upload

--- Main_script.m
+++ Main_script.m
@@ -4,14 +4,14 @@ A=A/255;
 A=imresize(A,[512,512]);
 img_size=size(A);
 %rasterscan(image,height,width,square size,step size)
-T=rasterscan(A,512,512,64,16);
+T=rasterscan(A,512,512,64,32);
 
 %initWH(T matrix, user defined 'R',gap while initial centroids(1or2), k means iterations);
-[W,H]=initWH(T,380,2,10);
+[W,H]=initWH(T,100,2,10);
 
-[C,D]=nmf(T,W,H,1e-3,10000,1000);
+[C,D]=nmf(T,W,H,1e-3,1000,400);
 Tf=C*D;
 %recreate(T matrix,square size,step size,height,width);
-Af=recreate(Tf,64,16,512,512);
+Af=recreate(Tf,64,32,512,512);
 F=[Af A];
 imagesc(F);
